876c21c8f83c1935f7d4626e36c9bf8f841fd160
Stewart Rogers <Stewart.Rogers@celltrust.com>
2019-11-15 23:38:53 -0700

    Rapid Dev.

--- BadNetworkAttachedStorage.Shared/BadNasDirectoryInfo.cs
+++ BadNetworkAttachedStorage.Shared/BadNasDirectoryInfo.cs
@@ -6,13 +6,25 @@ namespace System.IO {
         private readonly DirectoryInfo DirectoryInfo;
         public BadNasDirectoryInfo(DirectoryInfo directoryInfo) => DirectoryInfo = directoryInfo;
 
-        public BadNasFileInfo[] GetFiles() =>
+        public BadNasFileInfo CreateFile(String fileName) =>
+              RetryIO.Retry(() => {
+                  DirectoryInfo.Refresh();
+                  var badNasFileInfo = new BadNasFileInfo(new FileInfo(Path.Combine(DirectoryInfo.FullName, fileName)));
+                  badNasFileInfo.Create();
+                  return badNasFileInfo;
+              });
+
+        public void Create() =>
+              RetryIO.Retry(() => {
+                  DirectoryInfo.Refresh();
+                  if (!DirectoryInfo.Exists)
+                      DirectoryInfo.Create();
+              });
+
+        public BadNasDirectoryInfo CreateSubdirectory(String path) =>
             RetryIO.Retry(() => {
                 DirectoryInfo.Refresh();
-                return DirectoryInfo.GetFiles()
-                    .OrderBy(fileInfo => fileInfo.Name)
-                        .Select(fileInfo => new BadNasFileInfo(fileInfo))
-                            .ToArray();
+                return new BadNasDirectoryInfo(DirectoryInfo.CreateSubdirectory(path));
             });
 
         public BadNasDirectoryInfo[] GetDirectories() =>
@@ -23,5 +35,78 @@ namespace System.IO {
                         .Select(directoryInfo => new BadNasDirectoryInfo(directoryInfo))
                             .ToArray();
              });
+
+        public BadNasDirectoryInfo[] GetDirectories(String searchPattern) =>
+            RetryIO.Retry(() => {
+                DirectoryInfo.Refresh();
+                return DirectoryInfo.GetDirectories(searchPattern)
+                   .OrderBy(directoryInfo => directoryInfo.Name)
+                       .Select(directoryInfo => new BadNasDirectoryInfo(directoryInfo))
+                           .ToArray();
+            });
+
+        public BadNasDirectoryInfo[] GetDirectories(String searchPattern, SearchOption searchOption) =>
+            RetryIO.Retry(() => {
+                DirectoryInfo.Refresh();
+                return DirectoryInfo.GetDirectories(searchPattern, searchOption)
+                   .OrderBy(directoryInfo => directoryInfo.Name)
+                       .Select(directoryInfo => new BadNasDirectoryInfo(directoryInfo))
+                           .ToArray();
+            });
+
+        public BadNasFileInfo[] GetFiles() =>
+          RetryIO.Retry(() => {
+              DirectoryInfo.Refresh();
+              return DirectoryInfo.GetFiles()
+                  .OrderBy(fileInfo => fileInfo.Name)
+                      .Select(fileInfo => new BadNasFileInfo(fileInfo))
+                          .ToArray();
+          });
+
+        public BadNasFileInfo[] GetFiles(String searchPattern, SearchOption searchOption) =>
+          RetryIO.Retry(() => {
+              DirectoryInfo.Refresh();
+              return DirectoryInfo.GetFiles(searchPattern, searchOption)
+                  .OrderBy(fileInfo => fileInfo.Name)
+                      .Select(fileInfo => new BadNasFileInfo(fileInfo))
+                          .ToArray();
+          });
+
+        public BadNasFileInfo[] GetFiles(String searchPattern) =>
+          RetryIO.Retry(() => {
+              DirectoryInfo.Refresh();
+              return DirectoryInfo.GetFiles(searchPattern)
+                  .OrderBy(fileInfo => fileInfo.Name)
+                      .Select(fileInfo => new BadNasFileInfo(fileInfo))
+                          .ToArray();
+          });
+
+        public void Delete() =>
+            RetryIO.Retry(() => {
+                DirectoryInfo.Refresh();
+                DirectoryInfo.Delete();
+            });
+
+        public void Delete(Boolean recursive) =>
+            RetryIO.Retry(() => {
+                DirectoryInfo.Refresh();
+                DirectoryInfo.Delete(recursive);
+            });
+
+        public String Name {
+            get =>
+                RetryIO.Retry(() => {
+                    DirectoryInfo.Refresh();
+                    return DirectoryInfo.Name;
+                });
+        }
+
+        public String FullName {
+            get =>
+                RetryIO.Retry(() => {
+                    DirectoryInfo.Refresh();
+                    return DirectoryInfo.FullName;
+                });
+        }
     }
 }
--- BadNetworkAttachedStorage.Shared/BadNasFileInfo.cs
+++ BadNetworkAttachedStorage.Shared/BadNasFileInfo.cs
@@ -1,4 +1,6 @@
 ﻿using System.IO.Library;
+using System.Linq;
+using System.Text;
 
 namespace System.IO {
     public class BadNasFileInfo {
@@ -6,11 +8,92 @@ namespace System.IO {
 
         public BadNasFileInfo(FileInfo fileInfo) => FileInfo = fileInfo;
 
-        public Boolean Exists() =>
-             RetryIO.Retry(() => {
-                 FileInfo.Refresh();
-                 return FileInfo.Exists;
-             });
-    }
+        public Boolean IsReadOnly {
+            get =>
+                RetryIO.Retry(() => {
+                    FileInfo.Refresh();
+                    return FileInfo.IsReadOnly;
+                });
+        }
 
+        public void Create() =>
+          RetryIO.Retry(() => {
+              FileInfo.Refresh();
+              if (!FileInfo.Exists)
+                  FileInfo.Create();
+          });
+
+        public Boolean Exists() =>
+            RetryIO.Retry(() => {
+                FileInfo.Refresh();
+                return FileInfo.Exists;
+            });
+
+        public String Name {
+            get =>
+                RetryIO.Retry(() => {
+                    FileInfo.Refresh();
+                    return FileInfo.Name;
+                });
+        }
+
+        public String FullName {
+            get =>
+                RetryIO.Retry(() => {
+                    FileInfo.Refresh();
+                    return FileInfo.FullName;
+                });
+        }
+
+
+        public void AppendAllText(String contents) =>
+            RetryIO.Retry(() => {
+                FileInfo.Refresh();
+                File.AppendAllText(FileInfo.FullName, contents);
+            });
+
+        public void AppendAllText(String contents, Encoding encoding) =>
+            RetryIO.Retry(() => {
+                FileInfo.Refresh();
+                File.AppendAllText(FileInfo.FullName, contents, encoding);
+            });
+
+        public void WriteAllText(String contents) =>
+           RetryIO.Retry(() => {
+               FileInfo.Refresh();
+               File.WriteAllText(FileInfo.FullName, contents);
+           });
+
+        public void WriteAllText(String contents, Encoding encoding) =>
+            RetryIO.Retry(() => {
+                FileInfo.Refresh();
+                File.WriteAllText(FileInfo.FullName, contents, encoding);
+            });
+
+
+
+        public String ReadAllText() =>
+              RetryIO.Retry(() => {
+                  FileInfo.Refresh();
+                  return File.ReadAllText(FileInfo.FullName);
+              });
+
+        public String ReadAllText(Encoding encoding) =>
+              RetryIO.Retry(() => {
+                  FileInfo.Refresh();
+                  return File.ReadAllText(FileInfo.FullName, encoding);
+              });
+
+        public String[] ReadLines() =>
+              RetryIO.Retry(() => {
+                  FileInfo.Refresh();
+                  return File.ReadLines(FileInfo.FullName).ToArray();
+              });
+
+        public String[] ReadLines(Encoding encoding) =>
+              RetryIO.Retry(() => {
+                  FileInfo.Refresh();
+                  return File.ReadLines(FileInfo.FullName, encoding).ToArray();
+              });
+    }
 }
