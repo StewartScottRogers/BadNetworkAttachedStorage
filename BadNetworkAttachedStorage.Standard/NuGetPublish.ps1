11d149f7dcb1a52adc630cc5f43ea2e6b6c76cfd
Stewart Rogers <Stewart.Rogers@celltrust.com>
2019-11-17 22:04:27 -0700

    Rapid Dev.

--- BadNetworkAttachedStorage.Standard/NuGetPublish.ps1
+++ BadNetworkAttachedStorage.Standard/NuGetPublish.ps1
@@ -1,3 +1,3 @@
 $ScriptDir = Split-Path $script:MyInvocation.MyCommand.Path
 Write-Host "Current script directory is $ScriptDir"
-dotnet nuget push $ScriptDir\bin\Release\BadNetworkAttachedStorage.Standard.1.0.2.nupkg -k oy2kpgvvvnvqe55fcg4qg2hmgfln5cieziv56wlxksyvj4 -s https://api.nuget.org/v3/index.json
+dotnet nuget push $ScriptDir\bin\Release\BadNetworkAttachedStorage.Standard.1.0.3.nupkg -k oy2kpgvvvnvqe55fcg4qg2hmgfln5cieziv56wlxksyvj4 -s https://api.nuget.org/v3/index.json
